19074f4d7a641681e044d6661810757f9f92391a
Arun Sonti <SontiV@Aetna.com>
2022-01-25 13:25:01 -0500

    add env variables

--- src/main/java/com/arun/argocdexample/Controller.java
+++ src/main/java/com/arun/argocdexample/Controller.java
@@ -1,13 +1,21 @@
 package com.arun.argocdexample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @Value("${app.name}")
+    private String name;
+
+
+    @Value("${app.teamName}")
+    private String teamName;
+
     @RequestMapping(path = "/")
     public String greetings() {
-        return "Hello from Arun";
+        return "Hello all, This is " + name + " from team " + teamName;
     }
 }
